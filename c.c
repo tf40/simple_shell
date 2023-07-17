735106d77aff6fcf926aae17c5a5bb2ef6447e12
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-17 21:30:51 +0000

    Ouuu

---
+++ c.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @ac: No. of CLI args.
+ * @av: array of pointers to CLI args
+ * Return: 0 always.
+ */
+int main(int ac, char *av[])
+{
+	char *cmd_line = NULL, **args = NULL;
+	size_t size_line;
+	pid_t child_pid;
+	int status, i;
+
+	(void) i;
+	(void) ac;
+	for (;;)
+	{
+		write(1, "#cisfun$ ", 9);
+		if (getline(&cmd_line, &size_line, stdin) < 0)
+			break;
+		if (*cmd_line == EOF)
+			break;
+		cmd_line[strlen(cmd_line) - 1] = '\0';
+
+		args = malloc(sizeof(char *) * 2);
+		args[0] = malloc(sizeof(char) * (strlen(cmd_line) + 1));
+		strcpy(args[0], cmd_line);
+		args[1] = NULL;
+		child_pid = fork();
+		if (child_pid < 0)
+			perror("Error: ");
+		if (child_pid == 0)
+		{
+			execve(args[0], args, environ);
+			perror(av[0]);
+		}
+		wait(&status);
+		for (i = 0; i < 2; i++)
+			free(args[i]);
+	}
+	free(args);
+	free(cmd_line);
+	write(1, "\n", 1);
+	exit(0);
+}
---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* Variables declarations */
+extern char *environ[];
+
+/* Functions prototypes */
+ssize_t _getline(char **line, size_t *n, int fd);
+#endif
